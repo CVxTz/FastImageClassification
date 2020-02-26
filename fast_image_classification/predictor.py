74a8e76d0b231a00e119c3ed93c050eb29755b99
jenazzad <mansaryouness@gmail.com>
2020-02-26 22:00:22 +0100

    working predictor

--- fast_image_classification/predictor.py
+++ fast_image_classification/predictor.py
@@ -1,18 +1,22 @@
-import yaml
+import argparse
 
+import numpy as np
+import yaml
 from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
 from tensorflow.keras.models import load_model
+
+from preprocessing_utilities import read_img_from_path, resize_img, read_from_file
 
 
 class ImagePredictor:
     def __init__(
-        self, model_path, resize_size, classes, pre_processing_function=preprocess_input
+        self, model_path, resize_size, targets, pre_processing_function=preprocess_input
     ):
         self.model_path = model_path
         self.pre_processing_function = pre_processing_function
         self.model = load_model(self.model_path)
         self.resize_size = resize_size
-        self.classes = classes
+        self.targets = targets
 
     @classmethod
     def init_from_config_path(cls, config_path):
@@ -21,6 +25,55 @@ class ImagePredictor:
         predictor = cls(
             model_path=config["model_path"],
             resize_size=config["resize_shape"],
-            classes=config["classes"],
+            targets=config["targets"],
         )
         return predictor
+
+    def predict_from_array(self, arr):
+        arr = resize_img(arr, h=self.resize_size[0], w=self.resize_size[1])
+        arr = self.pre_processing_function(arr)
+        pred = self.model.predict(arr[np.newaxis, ...]).ravel().tolist()
+        pred = [round(x, 3) for x in pred]
+        return {k: v for k, v in zip(self.targets, pred)}
+
+    def predict_from_path(self, path):
+        arr = read_img_from_path(path)
+        return self.predict_from_array(arr)
+
+    def predict_from_file(self, file_object):
+        arr = read_from_file(file_object)
+        return self.predict_from_array(arr)
+
+
+if __name__ == "__main__":
+    """
+    python train.py --predictor_config "../example/predictor_config.yaml"
+
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--predictor_config_path",
+        help="predictor_config_path",
+        default="../example/predictor_config.yaml",
+    )
+
+    args = parser.parse_args()
+
+    predictor_config_path = args.predictor_config_path
+
+    predictor = ImagePredictor.init_from_config_path(predictor_config_path)
+
+    # pred = predictor.predict_from_path(
+    #     "../example/data/0a42098532a047e0b20f8dc22a9c1d6f.jpg"
+    # )
+    # print(pred)
+    #
+    # pred = predictor.predict_from_path(
+    #     "../example/data/0ab5f5ba8484483a8f8003c0152e181d.jpg"
+    # )
+    # print(pred)
+    #
+    # with open("../example/data/0ab5f5ba8484483a8f8003c0152e181d.jpg", "rb") as f:
+    #     _pred = predictor.predict_from_file(f)
+    #
+    # print(_pred)
--- fast_image_classification/preprocessing_utilities.py
+++ fast_image_classification/preprocessing_utilities.py
@@ -1,9 +1,17 @@
 import cv2
+import numpy as np
 
 
 def read_img_from_path(path):
-    img = cv2.imread(path)
+    img = cv2.imread(path, cv2.IMREAD_COLOR)
     return img
+
+
+def read_from_file(file_object):
+    arr = np.fromstring(file_object.read(), np.uint8)
+    img_np = cv2.imdecode(arr, cv2.IMREAD_COLOR)
+
+    return img_np
 
 
 def resize_img(img, h=128, w=128):
